d859fcd51c173b91342382a6865c9651398342a0
leonelmedova <ldavid.mdn@gmail.com>
2024-02-07 17:47:50 -0500

    Clase de arranque finalizada

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+// echo "Hola. Estoy programando con PHP \n";
+
+$nombre = "Carlos";
+$apellido = "Castro";
+
+// echo "No, no. Yo me llamo " . $nombre . " " . $apellido .  "\n";
+
+// echo "No, no. Yo me llamo $nombre $apellido \n";
+
+echo "El resultado de 4 x 5 es " . (4 * 5) . "\n";
