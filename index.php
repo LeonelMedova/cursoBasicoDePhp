6f0832761913a080d987d999930a6755dae4caf1
leonelmedova <ldavid.mdn@gmail.com>
2024-02-07 18:02:41 -0500

    F cl11

--- index.php
+++ index.php
@@ -9,4 +9,25 @@ $apellido = "Castro";
 
 // echo "No, no. Yo me llamo $nombre $apellido \n";
 
-echo "El resultado de 4 x 5 es " . (4 * 5) . "\n";
+// echo "El resultado de 4 x 5 es " . (4 * 5) . "\n";
+
+$persona = [
+    "Carlos" => 22,
+    "Revendedor" => 35,
+    "Juan" => 36
+];
+
+// var_dump( $persona );
+// print_r( $persona );
+
+// Esto es un comentario
+# Esto es otro comentario
+
+/* 
+Cualquier cosa,
+Puedo seguir escribiendo 
+si
+
+*/
+
+echo "\n";
