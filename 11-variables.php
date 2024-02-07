7750a2ee7792cca06d9f4dbe03bca5412360ec03
leonelmedova <ldavid.mdn@gmail.com>
2024-02-07 18:25:15 -0500

    F cl11-2 error anterior

---
+++ 11-variables.php
@@ -0,0 +1,17 @@
+<?php
+
+$numero_1 = 8;
+$numero_2 = 7;
+// $numero_1 = 5;  cambia por que es una var
+
+echo $numero_1 + $numero_2;
+
+echo "\n";
+
+define("NUMERO_PI", 3.14); // Forma de declarar constantes
+
+echo "\n";
+
+echo NUMERO_PI;
+
+echo "\n";
